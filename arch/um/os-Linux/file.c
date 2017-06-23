db0f9fc390b7bd64787094e11936630aaf2f9977
Thomas Meyer <thomas@m3y3r.de>
2017-06-23 10:34:46 +0200

    um: v2: Fix gcc 7 sys/sysmacros.h warning
    Original warning was:
    
    arch/um/os-Linux/file.c:600:13: warning: In the GNU C Library, "major" is defined
     by <sys/sysmacros.h>. For historical compatibility, it is
     currently defined by <sys/types.h> as well, but we plan to
     remove this soon. To use "major", include <sys/sysmacros.h>
     directly. If you did not intend to use a system-defined macro
     "major", you should undefine it after including <sys/types.h>.
      return major(dev);
    
    Signed-off-by: Thomas Meyer <thomas@m3y3r.de>

--- arch/um/os-Linux/file.c
+++ arch/um/os-Linux/file.c
@@ -14,6 +14,7 @@
 #include <sys/stat.h>
 #include <sys/un.h>
 #include <sys/types.h>
+#include <sys/sysmacros.h>
 #include <os.h>
 
 static void copy_stat(struct uml_stat *dst, const struct stat64 *src)
